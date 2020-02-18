c8904ff5b4fd848e915e67ee5483a3a62599df85
Graham <herdmanctg@outlook.com>
2020-02-19 07:39:54 +1100

    use terser-plugin to minify webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -36,5 +37,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new TerserPlugin()]
 };
