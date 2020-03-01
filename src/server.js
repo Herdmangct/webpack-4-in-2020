14b909d9efafcd0f213504474c9bc70bc1f27510
Graham <herdmanctg@outlook.com>
2020-03-02 08:05:27 +1100

    use node server together with webpack - work in progress

--- src/server.js
+++ src/server.js
@@ -3,11 +3,19 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 
-app.get("/", function(request, result) {
+app.get("/hello-world/", function(request, result) {
   const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
   const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
   result.send(contentFromHtmlFile);
 });
+
+app.get("/yosemite/", function(request, result) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/yosemite.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  result.send(contentFromHtmlFile);
+});
+
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
 
 app.listen(3000, function() {
   console.log("The app is up and running at http://localhost:3000");
