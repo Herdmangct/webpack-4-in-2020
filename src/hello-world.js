f44454609338967ed7ed3528eec18caefd1a5b0f
Graham <herdmanctg@outlook.com>
2020-03-05 08:37:57 +1100

    use fontawesome with webpack

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,7 +1,12 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import "./index.scss";
 import HelloWorldButton from "./components/hello-world-button";
 import Heading from "./components/heading/heading.js";
 import "bootstrap";
+
+library.add(faSpinner);
+dom.watch();
 
 const heading = new Heading();
 heading.render("Hello, world!");
